5a67e05346a50eeb704e3e21569853dbd10d3cf5
Jinyang88 <liujy725@gmail.com>
2019-07-17 16:35:19 +0800

    update sh

--- train_snli.sh
+++ train_snli.sh
@@ -4,7 +4,7 @@
 #SBATCH -p gpu_24h
 
 cd scripts/training/
-python train_snli.py -gpu 0 -id 0 $ python train_snli.py -lr 0.0001 -gpu 1 -id 6
+python train_snli.py -gpu 0 -id 0 & python train_snli.py -lr 0.0001 -gpu 1 -id 6
 python train_snli.py -use_attention "" -gpu 0 -id 1 & python train_snli.py -use_second_lstm "" -gpu 1 -id 2
 python train_snli.py -scheduler "" -gpu 0 -id 4 & python train_snli.py -use_final_tanh "" -gpu 1 -id 5
 
